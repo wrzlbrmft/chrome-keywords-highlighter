adb7ac59c1fc16a047a4ece983670d1b356b3884
matthias <matthias@hobbes>
2015-04-13 22:20:16 +0200

    now highlighting keywords

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ This software is distributed under the terms of the
 [GNU General Public License v3](https://www.gnu.org/licenses/gpl-3.0.en.html).
 
 Based on an earlier Chrome extension named
-*[Keywords Highlighter](https://code.google.com/p/keywords-highlighter/)*,
-distributed under the terms of the
-[MIT License](http://opensource.org/licenses/MIT).
+*[Keywords Highlighter](https://code.google.com/p/keywords-highlighter/)* which
+was also using
+[highlight](http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html)
+by [Johann Burkard](http://johannburkard.de/), both distributed under the terms
+of the [MIT License](http://opensource.org/licenses/MIT).
--- src/content.js
+++ src/content.js
@@ -1,5 +1,43 @@
 function keywordsHighlighter(options) {
-	document.body.style.backgroundColor = options.keywords;
+	// Based on "highlight: JavaScript text higlighting jQuery plugin" by Johann Burkard.
+	// http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html
+	// MIT license.
+	function highlight(node, pos, keyword) {
+		var span = document.createElement("span");
+		span.className = "highlighted";
+
+		var highlighted = node.splitText(pos);
+		/*var afterHighlighted = */highlighted.splitText(keyword.length);
+		var highlightedClone = highlighted.cloneNode(true);
+
+		span.appendChild(highlightedClone);
+		highlighted.parentNode.replaceChild(span, highlighted);
+	}
+
+	function addHighlights(node, keywords) {
+		var skip = 0;
+
+		if (3 == node.nodeType) {
+			for (var i = 0; i < keywords.length; i++) {
+				var keyword = keywords[i].toLowerCase();
+				var pos = node.data.toLowerCase().indexOf(keyword);
+				if (0 <= pos) {
+					highlight(node, pos, keyword);
+					skip = 1;
+				}
+			}
+		}
+		else if (1 == node.nodeType && !/(script|style)/i.test(node.tagName) && node.childNodes) {
+			for (var j = 0; j < node.childNodes.length; j++) {
+				j += addHighlights(node.childNodes[j], keywords);
+			}
+		}
+
+		return skip;
+	}
+
+	var keywords = options.keywords.split(",");
+	addHighlights(document.body, keywords);
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
