14a127042a3bc2b1335f4211afa0bc0bc12d3c32
matthias <matthias@hobbes>
2015-04-14 20:42:07 +0200

    grouped common functions

--- src/options.js
+++ src/options.js
@@ -1,19 +1,3 @@
-function loadOptions() {
-	if ("undefined" != typeof localStorage) {
-		document.getElementById("textKeywords").value = localStorage.getItem("keywords");
-		document.getElementById("colorColor").value = localStorage.getItem("color") || "#000000";
-		document.getElementById("colorBackgroundColor").value = localStorage.getItem("backgroundColor") || "#ffff00";
-	}
-}
-
-function saveOptions() {
-	if ("undefined" != typeof localStorage) {
-		localStorage.setItem("keywords", document.getElementById("textKeywords").value);
-		localStorage.setItem("color", document.getElementById("colorColor").value);
-		localStorage.setItem("backgroundColor", document.getElementById("colorBackgroundColor").value);
-	}
-}
-
 document.addEventListener("DOMContentLoaded", function() {
 	loadOptions();
 
